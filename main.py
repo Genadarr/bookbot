a4e07a4764a974b4afbfc29c8c366e1474c7e2e8
Genadarr <genadarr@gmail.com>
2024-03-06 02:14:29 -0500

    more updates

--- main.py
+++ main.py
@@ -26,6 +26,19 @@ def sort_on(d):
     return d["num"]
 
 # sort the dict based on highest number at the top
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+# def sort_on(dict):
+#     return dict["num"]
+
+# vehicles = [
+#     {"name": "car", "num": 7},
+#     {"name": "plane", "num": 10},
+#     {"name": "boat", "num": 2}
+# ]
+# vehicles.sort(reverse=True, key=sort_on)
+# print(vehicles)
+# [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
 def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list = []
     for ch in num_chars_dict:
